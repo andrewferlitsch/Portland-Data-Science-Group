f8fc5e9fd0d6a92db9e3016b7d44532e13a20f31
Andrew Ferlitsch <Andrew Ferlitsch>
2016-12-07 13:55:40 -0800

    finish backend receiving suggestions

--- TechnicalInterview/suggest.php
+++ TechnicalInterview/suggest.php
@@ -3,6 +3,11 @@ if( isset( $_POST['category'] ) ) {
 	$category = $_POST['category'];
 	$question = $_POST['question'];
 	$answer   = $_POST['answer'];
+	
+	$txt = "{ category:\"" . $category . "\"," . PHP_EOL . "question:\"" . $question . "\"," . PHP_EOL . "answer:\"" . $answer . "\"},";
+	$file = file_put_contents("suggest.json", $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+	fclose( $file );
+	
 	echo "Submitted";
 }
 ?>
