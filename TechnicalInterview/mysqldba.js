e9fa4ca74b40f8f11b60506060f1a1407ac25837
Andrew Ferlitsch <Andrew Ferlitsch>
2016-12-12 10:42:34 -0800

    fix scoring bug

--- TechnicalInterview/mysqldba.js
+++ TechnicalInterview/mysqldba.js
@@ -90,7 +90,7 @@ technical.controller( 'mysqlDbaCtrl', function( $scope ) {
 				  "<button class='w3-btn w3-teal w3-round w3-tiny' ng-click='answers=!answers'>Show Answers</button>" +
 				  "<ul>" +
 				  "	<li ng-repeat='question in random'><span class='w3-tooltip rank' name='mysqldba' id='{{question.id}}'>Rank {{question.rank}} <span class='w3-text w3-tag w3-khaki w3-round w3-small'>Click to Suggest Another Ranking</span></span> {{question.question}}<br/><br/>" +
-				  "	<span ng-show='answers'><span class='answer'>{{question.answer}}</span> <button class='w3-btn w3-khaki w3-round w3-small better' name='mysqldba' id='{{question.id}}'>Suggest A Better Answer</button><br/></span>Correct <input class='correct' type='checkbox'/><br/><br/></li>" +
+				  "	<span ng-show='answers'><span class='answer'>{{question.answer}}</span> <button class='w3-btn w3-khaki w3-round w3-small better' name='mysqldba' id='{{question.id}}'>Suggest A Better Answer</button><br/></span>Correct <input class='mysqldba-correct' type='checkbox'/><br/><br/></li>" +
 				  "</ul>" +
 				  "<button class='w3-btn w3-green score' name='mysqldba'>Score</button>" +
 				  "&nbsp;<button class='w3-btn w3-khaki w3-round w3-small question' name='mysqldba'>Suggest a Question</button>" +
--- TechnicalInterview/questions.js
+++ TechnicalInterview/questions.js
@@ -3,7 +3,7 @@ technical.controller( 'nameCtrl', function( $scope ) {
 	$scope.questions = [{ question: "",
 						  answer: "",
 						  rank: 1,
-						  id: 205
+						  id: 212
 						},
 						{ question: "",
 						  answer: "",
@@ -30,7 +30,7 @@ technical.controller( 'nameCtrl', function( $scope ) {
 				  "<button class='w3-btn w3-teal w3-round w3-tiny' ng-click='answers=!answers'>Show Answers</button>" +
 				  "<ul>" +
 				  "	<li ng-repeat='question in random'><span class='w3-tooltip rank' name='name' id='{{question.id}}'>Rank {{question.rank}} <span class='w3-text w3-tag w3-khaki w3-round w3-small'>Click to Suggest Another Ranking</span></span> {{question.question}}<br/><br/>" +
-				  "	<span ng-show='answers'><span class='answer'>{{question.answer}}</span> <button class='w3-btn w3-khaki w3-round w3-small better' name='name' id='{{question.id}}'>Suggest A Better Answer</button><br/></span>Correct <input class='correct' type='checkbox'/><br/><br/></li>" +
+				  "	<span ng-show='answers'><span class='answer'>{{question.answer}}</span> <button class='w3-btn w3-khaki w3-round w3-small better' name='name' id='{{question.id}}'>Suggest A Better Answer</button><br/></span>Correct <input class='name-correct' type='checkbox'/><br/><br/></li>" +
 				  "</ul>" +
 				  "<button class='w3-btn w3-green score' name='name'>Score</button>" +
 				  "&nbsp;<button class='w3-btn w3-khaki w3-round w3-small question' name='name'>Suggest a Question</button>" +
