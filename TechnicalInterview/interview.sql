07b6b6df3e121b2995179e4c9246dfea32b073b1
Andrew Ferlitsch <Andrew Ferlitsch>
2016-12-16 09:30:14 -0800

    SQL schema file

---
+++ TechnicalInterview/interview.sql
@@ -0,0 +1,14 @@
+drop table questions;
+create table questions
+(
+	id	 		int				not null primary key auto_increment,
+	category 	varchar(16)		not null,
+	question 	varchar(256)	not null,
+	answer   	varchar(256)	not null,
+	rank	 	smallint(30)	not null default 1,
+
+	index	(category),
+	index	(rank)
+)
+ENGINE = MyISAM CHARACTER SET = utf8 COLLATE utf8_bin;
+show columns in questions;
