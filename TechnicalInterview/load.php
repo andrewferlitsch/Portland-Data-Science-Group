f646dd4841605f64cfa54211df3ca1d0fbde2d0f
Andrew Ferlitsch <Andrew Ferlitsch>
2016-12-16 16:53:38 -0800

    backend hookup work

--- TechnicalInterview/load.php
+++ TechnicalInterview/load.php
@@ -1,6 +1,15 @@
 <?php
+function Load( $category ) {
+	echo "[ { \"id\": 1, \"rank\": 1, \"question\": \"one?\",   \"answer\": \"yes\"},
+			{ \"id\": 2, \"rank\": 3, \"question\": \"two?\",   \"answer\": \"no\" },
+   			{ \"id\": 3, \"rank\": 2, \"question\": \"three?\", \"answer\": \"yup\" }]";
+}
+
 $server_ip = $_SERVER['REMOTE_ADDR'];
 if( isset( $_POST['category'] ) ) {
-	echo $server_ip;
+	if ( $server_ip != "70.59.146.107" )
+		echo $server_ip;
+	else
+		Load( $_POST['category'] );
 }
 ?>
