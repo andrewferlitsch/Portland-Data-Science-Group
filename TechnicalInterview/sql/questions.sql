b13214658e9ae3db771b4e82dda3fd59a81769a3
Andrew Ferlitsch <Andrew Ferlitsch>
2016-12-27 18:10:34 -0800

    schema tuning

--- TechnicalInterview/sql/questions.sql
+++ TechnicalInterview/sql/questions.sql
@@ -5,7 +5,7 @@ create table questions
 	category 	varchar(16)		not null,
 	question 	varchar(256)	not null,
 	answer   	varchar(256)	not null,
-	rank	 	smallint(30)	not null default 1,
+	rank	 	tinyint		    not null default 1,
 
 	index	(category),
 	index	(rank)
@@ -18,3 +18,20 @@ source algorithms.sql;
 source angularjs.sql;
 source c.sql;
 source cplusplus.sql;
+source css.sql;
+source datascience.sql;
+source dos.sql;
+source html.sql;
+source html5.sql;
+source java.sql;
+source javascript.sql;
+source jquery.sql;
+source mysql.sql;
+source mysqldba.sql;
+source network.sql;
+source oop.sql;
+source os.sql;
+source python.sql;
+source r.sql;
+source security.sql;
+source web.sql;
